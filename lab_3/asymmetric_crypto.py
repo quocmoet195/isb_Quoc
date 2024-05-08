ea6ff7f98c56b461f99d0134d9afc2a8da3274e6
quocmoet1952002 <quocvan140519@gmail.com>
2024-05-08 22:50:18 +0400

    create functions encrypt and decrypt of symmetric key

--- lab_3/asymmetric_crypto.py
+++ lab_3/asymmetric_crypto.py
@@ -9,3 +9,24 @@ def generate_key_pair():
         return rsa.generate_private_key(public_exponent=65537, key_size=2048)
     except Exception as ex:
         raise Exception(f"ERROR!!{ex}")
+
+
+def encrypt_symmetric_key(private_key, symmetric_key):
+    try:
+        encrypted_key = private_key.public_key().encrypt(symmetric_key, 
+                                    padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), 
+                                                 algorithm=hashes.SHA256(), label=None))
+        return encrypted_key                                                                      
+    except Exception as ex:
+        raise Exception(f"ERROR!!{ex}")
+
+
+def decrypt_symmetric_key(private_key, encrypted_symmetric_key):
+    try:
+        return private_key.decrypt(encrypted_symmetric_key, asymmetric_padding.OAEP(
+            mgf=asymmetric_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        ))
+    except Exception as ex:
+        raise Exception(f"ERROR!!{ex}")
