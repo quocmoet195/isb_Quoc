e4a42fafa88024eeb1fb6a9a4fccdd9e47d7b26e
quocmoet1952002 <quocvan140519@gmail.com>
2024-05-26 21:52:29 +0400

    fix function check_card_number and create_card_number

--- lab_4/card.py
+++ lab_4/card.py
@@ -2,30 +2,31 @@ import hashlib
 import multiprocessing as mp
 
 
-def check_card_number(center_part: int, bins: list, last_four_numbers: int, original_hash: str) -> str:
+def check_card_number(args) -> str:
     """
-    Функция проверяет номер карты хешу соответствует или нет
-    original_hash: Хеш карты
-    last_four_numbers: Последние 4 цифры карты
-    Return: Номер карты
+    Checks if a card number matches the original hash.
+    args: Tuple containing center_part, bins, last_four_numbers, original_hash
     """
+    center_part, bins, last_four_numbers, original_hash = args
     for bin_part in bins:
         card_number = f"{bin_part}{str(center_part).zfill(6)}{last_four_numbers}"
         if hashlib.blake2b(card_number.encode()).hexdigest() == original_hash:
             return card_number
+    return None
 
 def create_card_number(original_hash: str, bins: list, last_four_numbers: str, core_number: int = mp.cpu_count()) -> str:
     """
-    Функция подбираем номер карты
-    original_hash: Хеш карты
-    bins: набор БИНов карты
-    last_four_numbers: Последние 4 цифры карты
-    core_number: Количество ядер
-    Return: Номер карты
+    Generates the card number by comparing hashes.
+    original_hash: Original hash of the card
+    bins: List of BINs (Bank Identification Numbers)
+    last_four_numbers: Last four digits of the card number
+    core_number: Number of CPU cores to use for multiprocessing
     """
-    with mp.Pool(core_number) as p:
-        args = [(i, bins, last_four_numbers, original_hash) for i in range(0, 999999)]
-        for result in p.starmap(check_card_number, args):
+    with mp.Pool(core_number) as pool:
+        args = [(i, bins, last_four_numbers, original_hash) for i in range(0, 1000000)]
+        results = pool.map(check_card_number, args)
+        for result in results:
             if result:
-                p.terminate() 
+                pool.terminate() 
                 return result
+    return None
