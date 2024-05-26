872db570e3a73d1e882137f1ec7a39c6798f5460
quocmoet1952002 <quocvan140519@gmail.com>
2024-05-26 23:39:39 +0400

    fix file card

--- lab_4/card.py
+++ lab_4/card.py
@@ -4,8 +4,8 @@ import multiprocessing as mp
 
 def check_card_number(args) -> str:
     """
-    Checks if a card number matches the original hash.
-    args: Tuple containing center_part, bins, last_four_numbers, original_hash
+    Проверяет, соответствует ли номер карты исходному хешу.
+    args: Кортеж, содержащий center_part, bins, last_four_numbers, original_hash
     """
     center_part, bins, last_four_numbers, original_hash = args
     for bin_part in bins:
@@ -16,11 +16,10 @@ def check_card_number(args) -> str:
 
 def create_card_number(original_hash: str, bins: list, last_four_numbers: str, core_number: int = mp.cpu_count()) -> str:
     """
-    Generates the card number by comparing hashes.
-    original_hash: Original hash of the card
-    bins: List of BINs (Bank Identification Numbers)
-    last_four_numbers: Last four digits of the card number
-    core_number: Number of CPU cores to use for multiprocessing
+    Генерирует номер карты путем сравнения хешей.    original_hash: Original hash of the card
+    bins: Список BINs
+    last_four_numbers: Последние четыре цифры номера карты
+    core_number: Количество ядер, используемых для многопроцессорной обработки
     """
     with mp.Pool(core_number) as pool:
         args = [(i, bins, last_four_numbers, original_hash) for i in range(0, 1000000)]
